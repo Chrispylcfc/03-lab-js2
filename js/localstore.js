cb9d4f502a25293338183b210f779f711e02bedc
christopherpowell <chrispylcfc@gmail.com>
2017-10-21 14:05:44 +0100

    Added colour change to form text

--- js/localstore.js
+++ js/localstore.js
@@ -3,21 +3,28 @@
   let myName = document.getElementById("my-name");
   let getName = document.getElementById("get-name");
   let userName = document.getElementById("user-name");
+  let familyName = document.getElementById("family-name");
+  let colour = document.getElementById("colour");
+  let bgColor = document.getElementById("bg-color");
   let clear = document.getElementById("clear-button");
   let nameStored = localStorage.name;
+  let colourStored = localStorage.color;
 
   // Displays the name in the console at this stage:
   console.log(`Name on page load: ${nameStored}`);
+  console.log(`Colour on page load: ${colourStored}`);
 
   if(nameStored) {
     // If there's a name in localStorage, use it:
     myName.innerHTML = `again ${nameStored}`;
+    bgColor.style.color = colourStored;
     console.log(`Name stored is: ${nameStored}`);
+    console.log(`Colour stored is: ${colourStored}`);
   }
   else {
     // There's no name in localStorage:
     myName.innerHTML = "stranger";
-    console.log(`No name stored`);
+    console.log(`No name & colour stored`);
   }
 
   function PerformGreeting() {
@@ -26,16 +33,20 @@
       userName.focus();
     }
     // Gets the name the user entered:
-    nameStored = userName.value;
+    nameStored = userName.value + " " + familyName.value;
+    colourStored = colour.value;
 
     // Shows the name in "my-name":
     myName.innerHTML = nameStored;
+    bgColor.style.color = colourStored;
 
     // Puts the name into localStorage:
     localStorage.name = nameStored;
+    localStorage.color = colourStored;
 
     // Displays the name in the console at the final stage:
     console.log(`New name stored: ${nameStored}`);
+    console.log(`New colour stored: ${colourStored}`);
     event.preventDefault();
     return false;
   }
